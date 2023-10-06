0f5089cc428a5722d0c7323ec69bf027af823647
Pepe <128159900+NotPietro@users.noreply.github.com>
2023-10-06 11:55:22 -0300

    Add files via upload

---
+++ Nova pasta/lojinha.js
@@ -0,0 +1,25 @@
+function compra(){
+    var opcao = prompt("Escolha uma opção de 1 a 5")
+
+    switch(Number(opcao)){
+        case 1:
+            alert("Pastel")
+            break;
+
+            case 2:
+                alert("Coxinha")
+                break;
+
+                case 3:
+                    alert("Coca-Cola")
+                    break;
+                    
+                    case 4:
+                        alert("Pamonha")
+                        break;
+
+                        case 5:
+                            alert("Bala")
+                            break;
+    }
+}
