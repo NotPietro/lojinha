643737aa9148b9e22e5986e90c056d587d32c079
Pepe <128159900+NotPietro@users.noreply.github.com>
2023-10-06 12:03:48 -0300

    Add files via upload

---
+++ lojinha do lulinha/lojinha.js
@@ -0,0 +1,25 @@
+function compra(){
+    var opcao = prompt("Escolha uma opção de 1 a 5")
+
+    switch(Number(opcao)){
+        case 1:
+            alert("Vocẽ pegou um Pastel!")
+            break;
+
+            case 2:
+                alert("Vocẽ pegou um Coxinha!")
+                break;
+
+                case 3:
+                    alert("Vocẽ pegou um Coca-Cola!")
+                    break;
+                    
+                    case 4:
+                        alert("Vocẽ pegou um Pamonha!")
+                        break;
+
+                        case 5:
+                            alert("Vocẽ pegou um Bala!")
+                            break;
+    }
+}
